5684b864ffb5be5aa7d3c31d2c33c123b3770300
Camilo <juancmaringcu@gmail.com>
2021-09-24 22:32:44 -0500

    create new product

--- lib/app/screens/home_screen.dart
+++ lib/app/screens/home_screen.dart
@@ -3,35 +3,39 @@ import 'package:provider/provider.dart';
 import 'package:u_commerce/app/screens/screens.dart';
 import 'package:u_commerce/app/widgets/widgets.dart';
 import 'package:u_commerce/data/datasources/data_sources.dart';
-
+import 'package:u_commerce/data/models/models.dart';
 
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     final productServices = Provider.of<ProductService>(context);
 
-    if(productServices.isLoading) return LoadingScreen();
+    if (productServices.isLoading) return LoadingScreen();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Productos'),
       ),
       body: ListView.builder(
-        itemCount: productServices.products.length,
-        itemBuilder: (_, int index) => GestureDetector(
-          child: ProductCard(product: productServices.products[index],),
-          onTap: () {
-            productServices.selectedProduct = productServices.products[index].copy();
-            Navigator.pushNamed(context, 'product');
-          },
-        )
-      ),
+          itemCount: productServices.products.length,
+          itemBuilder: (_, int index) => GestureDetector(
+                child: ProductCard(
+                  product: productServices.products[index],
+                ),
+                onTap: () {
+                  productServices.selectedProduct =
+                      productServices.products[index].copy();
+                  Navigator.pushNamed(context, 'product');
+                },
+              )),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {
+          productServices.selectedProduct =
+              new Product(available: false, name: '', price: 0);
+          Navigator.pushNamed(context, 'product');
+        },
         child: Icon(Icons.add),
       ),
-   );
+    );
   }
 }
